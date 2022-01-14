022a21b9d834730557f6a56375d486374c8181ac
Z253534 <z253534@corp.clarobr>
2022-01-14 10:43:42 -0300

    Domain model ,database seed

--- backeend/src/main/java/com/bruno/alves/dsmoviedevsuperior/DsmovieDevsuperiorApplication.java
+++ backeend/src/main/java/com/bruno/alves/dsmoviedevsuperior/DsmovieDevsuperiorApplication.java
@@ -1,13 +1,13 @@
-package com.devsuperior.dsmovie;
+package com.bruno.alves.dsmoviedevsuperior;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsmovieApplication {
+public class DsmovieDevsuperiorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DsmovieApplication.class, args);
+		SpringApplication.run(DsmovieDevsuperiorApplication.class, args);
 	}
 
 }
--- backeend/src/test/java/com/bruno/alves/dsmoviedevsuperior/DsmovieDevsuperiorApplicationTests.java
+++ backeend/src/test/java/com/bruno/alves/dsmoviedevsuperior/DsmovieDevsuperiorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.dsmovie;
+package com.bruno.alves.dsmoviedevsuperior;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DsmovieApplicationTests {
+class DsmovieDevsuperiorApplicationTests {
 
 	@Test
 	void contextLoads() {
